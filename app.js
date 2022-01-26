65dbaa181c1e76d6c833e3bc5597936671516a97
muirurii <pnkanuna@gmail.com>
2022-01-26 18:06:16 +0300

    added more hoer effects

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-AOS.init({
-    offset: 200,
-    duration: 800,
-    easing: "ease-in-out",
-    mirror: true
-});
+    //AOS LIBRARY
+    AOS.init({
+        offset: 200,
+        duration: 800,
+        easing: "ease-in-out",
+        mirror: true
+    });
+
+    //Hero text animation
+
+    const allTexts = document.querySelectorAll('.texts p');
+
+    const rotateTexts = () => {
+        const active = document.querySelector('.active');
+        active.classList.remove('active');
+        active.nextElementSibling ? active.nextElementSibling.classList.add('active') : allTexts[0].classList.add('active');
+    }
+
+    setInterval(() => rotateTexts(), 3000)
